d26d4c5bc60d4b8f2e88e0a0d2f623e9c90d8cfb
Nikhil <nikhilbalamurugan@gmail.com>
2025-07-05 19:09:54 +0530

    style

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
 import Experience from './pages/Experience';
 import Achievements from './pages/Achievements';
 import Footer from './components/Footer';
-import Navbar from './components/Navbar';
 import AnimatedCursor from './components/AnimatedCursor';
 
 function App() {
   return (
     <Router>
-      <AnimatedCursor />
-      <Navbar />
-      <Routes>
-        <Route path="/" element={
-          <>
-            <Home />
-            <About />
-            <Projects />
-            <Experience />
-            <Achievements />
-            <Footer />
-          </>
-        } />
-      </Routes>
+      <div className="snap-y snap-mandatory overflow-y-scroll h-screen">
+        <AnimatedCursor />
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={
+              <>
+                <section id="home" className="snap-start"><Home /></section>
+                <section id="about" className="snap-start"><About /></section>
+                <section id="projects" className="snap-start"><Projects /></section>
+                <section id="experience" className="snap-start"><Experience /></section>
+                <section id="achievements" className="snap-start"><Achievements /></section>
+                <Footer />
+              </>
+            } />
+          </Routes>
+        </main>
+      </div>
     </Router>
   );
 }
-
-
 
 export default App;
